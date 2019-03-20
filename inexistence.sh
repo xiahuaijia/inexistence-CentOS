99c17649698000b95a59c2c508946fa8ef356e90
AceSheep <1012993608@qq.com>
2019-03-20 12:13:13 +0800

    测试 gpg

---
+++ inexistence.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# https://github.com/Aniverse/inexistence
+# Author: Aniverse AceSheep
+#
+# bash <(curl -s https://raw.githubusercontent.com/Aniverse/inexistence/master/inexistence.sh)
+# bash -c "$(wget --no-check-certificate -qO- https://github.com/Aniverse/inexistence/raw/master/inexistence.sh)"
+#
+# PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+# export PATH
+# --------------------------------------------------------------------------------
+SYSTEMCHECK=1
+DISABLE=0
+DeBUG=0
+INEXISTENCEVER=1.0.0
+INEXISTENCEDATE=2019.02.16
+script_lang=eng
+# --------------------------------------------------------------------------------
